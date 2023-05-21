3ff5e453a36af473015648ef0d154f4df01bc9cd
Gofrances <109204198+Gofrances@users.noreply.github.com>
2023-05-21 16:41:04 +0200

    Create Dump.sh
    Datei erstellt und getestet
    Wird mit "docker cp /home/vmadmin/Documents/DB_Container/dump.sh documents_db_1:/var/lib/mysql" in den Container kopiert
    
    Wird mit "docker exec documents_db_1 ls /var/lib/mysql" ausgeführt

---
+++ Dump.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# Script zur automatischen erzeugung von SQLdumps mit automatischer Benennung der Datei
+# Datenbankzugangsdaten 
+MYSQL_USER="root" 
+MYSQL_PASSWORD="Secret" 
+MYSQL_DATABASE="moodle" 
+# Verzeichnis, in dem die Sicherungsdatei gespeichert wird 
+BACKUP_DIR="var/lib/mysql" 
+# Dateinamen für die Sicherungsdatei 
+BACKUP_FILENAME="$MYSQL_DATABASE-$(date +%Y-%m-%d_%H-%M-%S).sql" 
+# MySQL-Dump-Befehl 
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $BACKUP_DIR/$BACKUP_FILENAME 
+# Erfolgsmeldung 
+echo "MySQL-Dump erfolgreich erstellt: $BACKUP_FILENAME"
