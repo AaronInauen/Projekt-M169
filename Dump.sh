d4d42fe7091586ce24de7e1874ec70dda8a7329b
Gofrances <109204198+Gofrances@users.noreply.github.com>
2023-05-23 19:30:22 +0200

    Dateiname mit Zeitzone versehen

--- Dump.sh
+++ Dump.sh
@@ -7,7 +7,7 @@ MYSQL_DATABASE="moodle"
 # Verzeichnis, in dem die Sicherungsdatei gespeichert wird 
 BACKUP_DIR="var/lib/mysql" 
 # Dateinamen für die Sicherungsdatei 
-BACKUP_FILENAME="$MYSQL_DATABASE-$(date +%Y-%m-%d_%H-%M-%S).sql" 
+BACKUP_FILENAME="$MYSQL_DATABASE-$(date +%Y-%m-%d_%H-%M-%S)-UTC.sql" 
 # MySQL-Dump-Befehl 
 mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $BACKUP_DIR/$BACKUP_FILENAME 
 # Erfolgsmeldung 
