464ae57955a3fc4135d7be2042e807e6a65df8ed
squeeze123 <severin.schneider@edu.gbssg.ch>
2023-05-24 14:26:02 +0200

    Sicherheit erhöht
    Passwörter aus dem File genommen.
    dump umbennent

--- script.sh
+++ script.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+# Passwörter einlesen
+read -p "Bitte geben Sie das Passwort für die Docker-DB ein: " password
+read -p "Bitte geben Sie das Passwort für die lokale-DB ein: " password2
+
 # Docker Compose ausführen
 docker-compose up -d
 
 # Warten, bis die Container gestartet sind
-sleep 10
-
-# MySQL-Dump erstellen
-sudo mysqldump --password='Riethuesli>12345' moodle > dump.sql
-
-# Dump in den MySQL-Container laden
-docker cp /home/vmadmin/dump4-2.sql vmadmin_db_1:/var/lib/mysql
-
 sleep 300
 
-docker exec -i vmadmin_db_1 -c "mysql -u root --password=root -e 'DROP DATABASE moodle; CREATE DATABASE moodle;'"
+# MySQL-Dump erstellen
+sudo mysqldump --password=$password2 moodle > dump4-1-2.sql
+
+# Dump in den MySQL-Container laden
+docker cp /home/vmadmin/dump4-1-2.sql vmadmin_db_1:/var/lib/mysql
+
+sleep 10
+
+docker exec -i vmadmin_db_1 bash -c "mysql -u root --password=$password -e 'DROP DATABASE moodle; CREATE DATABASE moodle;'"
 
 # In den MySQL-Container wechseln und den Dump importieren
-docker exec -i vmadmin_db_1 -u root --password=root moodle < ./dump.sql
+docker exec -i vmadmin_db_1 mysql -u root --password=$password moodle < ./dump4-1-2.sql
 
 # Aufräumen: Lokalen Dump entfernen
-rm dump.sql
+rm dump4-1-2.sql
