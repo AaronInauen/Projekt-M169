101b5eb1744ad465ffe3ab541892997ed6a2d815
squeeze123 <severin.schneider@edu.gbssg.ch>
2023-05-24 15:48:28 +0200

    Sleep & Namen + Cronjob erstellt
    Zusätzliches Sleep und die Containernamen angepasst
    Cronjob wird auch erstellt

--- script.sh
+++ script.sh
@@ -13,15 +13,25 @@ sleep 300
 # MySQL-Dump erstellen
 sudo mysqldump --password=$password2 moodle > dump4-1-2.sql
 
+sleep 30
+
 # Dump in den MySQL-Container laden
-docker cp /home/vmadmin/dump4-1-2.sql vmadmin_db_1:/var/lib/mysql
+docker cp /home/vmadmin/NewMoodle/dump4-1-2.sql newmoodle_db_1:/var/lib/mysql
 
 sleep 10
 
-docker exec -i vmadmin_db_1 bash -c "mysql -u root --password=$password -e 'DROP DATABASE moodle; CREATE DATABASE moodle;'"
+docker exec -i newmoodle_db_1 bash -c "mysql -u root --password=$password -e 'DROP DATABASE moodle; CREATE DATABASE moodle;'"
 
 # In den MySQL-Container wechseln und den Dump importieren
-docker exec -i vmadmin_db_1 mysql -u root --password=$password moodle < ./dump4-1-2.sql
+docker exec -i newmoodle_db_1 mysql -u root --password=$password moodle < ./dump4-1-2.sql
 
 # Aufräumen: Lokalen Dump entfernen
 rm dump4-1-2.sql
+
+# Cronjobskript wird eingefügt
+docker cp /home/vmadmin/NewMoodle/Dump.sh newmoodle_db_1:/var/lib/mysql
+
+# Cronjob wird erstellt
+sudo su <<suON
+echo "0 22 * * * docker exec newmoodle_db_1 /var/lib/mysql/Dump.sh" &>>/var/spool/cron/crontabs/root
+suON
